9ec5832956f0736254a640c001f981d5d5f5bcfa
like <980650920@qq.com>
2021-08-13 11:44:46 +0800

    lru 实现

---
+++ cache-gee/cache/lru.go
@@ -0,0 +1,81 @@
+package cache
+
+import (
+	"container/list"
+)
+
+type Cache struct {
+	maxBytes int64                    // 当前允许的最大内存
+	nBytes   int64                    // 已经使用的内存
+	ll       *list.List               // 双向链表
+	cache    map[string]*list.Element // 字典 key:字符串，value:对应链表中的节点
+
+	OnEvicted func(key string, value Value)
+}
+
+func New(maxBytes int64, onEvicted func(key string, value Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// entry list 存储的数据类型
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+// Get 根据key返回缓存内容，并移动到头部
+// 规定头部是使用的较多的
+// 尾部是最近使用较少的
+func (c Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// RemoveOldest 移除最近使用最少的，即移除队尾的
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add adds a value to the cache.
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nBytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
+
+// Len the number of cache entries
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
---
+++ cache-gee/go.mod
@@ -0,0 +1,3 @@
+module cache-gee
+
+go 1.16
