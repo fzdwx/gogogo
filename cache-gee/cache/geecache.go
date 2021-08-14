f725cc392903f6011487e2c9eff68d6267584994
like <980650920@qq.com>
2021-08-14 18:56:58 +0800

    分布式缓存 添加分布式节点

--- cache-gee/cache/geecache.go
+++ cache-gee/cache/geecache.go
@@ -26,6 +26,7 @@ type Group struct {
 	name      string // namespace
 	getter    Getter // 缓存未命中时的操作
 	mainCache cache  // 带锁的缓存
+	peers     PeerPicker
 }
 
 var (
@@ -78,7 +79,24 @@ func (g *Group) Get(key string) (ByteView, error) {
 	return g.load(key)
 }
 
-func (g *Group) load(key string) (ByteView, error) {
+// RegisterPeers registers a PeerPicker for choosing remote peer
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err := g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+			log.Println("[GeeCache] Failed to get from peer", err)
+		}
+	}
+
 	return g.getLocally(key)
 }
 
@@ -99,4 +117,13 @@ func (g *Group) getLocally(key string) (ByteView, error) {
 
 func (g *Group) populateCache(key string, v ByteView) {
 	g.mainCache.add(key, v)
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
 }
--- cache-gee/cache/http.go
+++ cache-gee/cache/http.go
@@ -2,9 +2,14 @@ package cache
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
+
+	"cache/consistenthash"
 )
 
 /*
@@ -12,13 +17,19 @@ import (
   /bathPath/{groupName}/{key}
 */
 
-const defaultBasePath = "/distributed_cache/"
-const pattern = "/bathPath/{groupName}/{key}"
+const (
+	defaultBasePath = "/distributed_cache/"
+	defaultReplicas = 50
+	pattern         = "/bathPath/{groupName}/{key}"
+)
 
 type HttpPool struct {
 	// eg: http://localhost:9999/distributed_cache/
-	self     string // 记录自己的地址 ip:port or website url
-	basePath string // 作为通信地址的开头，用于节点的访问
+	self        string // 记录自己的地址 ip:port or website url
+	basePath    string // 作为通信地址的开头，用于节点的访问
+	mtx         sync.Mutex
+	peers       *consistenthash.Map    // 一致性哈希算法的 Map，用来根据具体的 key 选择节点。
+	httpGetters map[string]*httpGetter // key: http://localhost:9999
 }
 
 func NewHttpPool(self string) *HttpPool {
@@ -28,30 +39,54 @@ func NewHttpPool(self string) *HttpPool {
 	}
 }
 
+func (h *HttpPool) Set(peers ...string) {
+	h.mtx.Lock()
+	defer h.mtx.Unlock()
+
+	h.peers = consistenthash.New(defaultReplicas, nil)
+	h.peers.Add(peers...)
+	h.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		h.httpGetters[peer] = &httpGetter{baseURL: peer + h.basePath}
+	}
+}
+
+// PickPeer picks a peer according to key
+func (h *HttpPool) PickPeer(key string) (peer PeerGetter, ok bool) {
+	h.mtx.Lock()
+	defer h.mtx.Unlock()
+
+	if peer := h.peers.Get(key); peer != "" && peer != h.self {
+		h.Log("Pick peer %s", peer)
+		return h.httpGetters[peer], true
+	}
+	return nil, false
+}
+
 // Log info with server name
-func (p *HttpPool) Log(format string, v ...interface{}) {
-	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+func (h *HttpPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", h.self, fmt.Sprintf(format, v...))
 }
 
 // ServeHTTP impl http url handler
 // only handler starWith HttpPool.basePath
-func (p *HttpPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *HttpPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	method := r.Method
-	if !strings.HasPrefix(path, p.basePath) {
+	if !strings.HasPrefix(path, h.basePath) {
 		panic("HttpPool serving unexpected path: " + path)
 	}
-	p.Log("%s %s", method, path)
+	h.Log("%s %s", method, path)
 
 	// GET /bathPath/{groupName}/{key}
-	if strings.HasPrefix(path, p.basePath) && method == http.MethodGet {
-		p.handlerGet(path, w, r)
+	if strings.HasPrefix(path, h.basePath) && method == http.MethodGet {
+		h.handlerGet(path, w, r)
 	}
 }
 
-func (p *HttpPool) handlerGet(path string, w http.ResponseWriter, r *http.Request) {
+func (h *HttpPool) handlerGet(path string, w http.ResponseWriter, r *http.Request) {
 	// see pattern
-	parts := strings.SplitN(path[len(p.basePath):], "/", 2)
+	parts := strings.SplitN(path[len(h.basePath):], "/", 2)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
@@ -74,3 +109,34 @@ func (p *HttpPool) handlerGet(path string, w http.ResponseWriter, r *http.Reques
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+type httpGetter struct {
+	baseURL string
+}
+
+// Get  用于从对应 group 查找缓存值。
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", resp.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
